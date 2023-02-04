e23fa770d5ddc8793828bc498681dd7a9ee3cf88
Javan Chen <imjavanchen@gmail.com>
2023-02-04 17:13:10 +0800

    Fix MockERC20 to ERC20

--- test/SpenderPermit2ERC20.t.sol
+++ test/SpenderPermit2ERC20.t.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.0;
 
 import {Test} from 'forge-std/Test.sol';
 import {SafeERC20, IERC20} from 'openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol';
+import {ERC20} from 'openzeppelin-contracts/contracts/token/ERC20/ERC20.sol';
 import {Router, IRouter} from '../src/Router.sol';
 import {SpenderPermit2ERC20, ISpenderPermit2ERC20, ISignatureTransfer, IAllowanceTransfer} from '../src/SpenderPermit2ERC20.sol';
-import {MockERC20} from './mocks/MockERC20.sol';
 import {PermitSignature} from "./utils/PermitSignature.sol";
 import {EIP712} from 'permit2/EIP712.sol';
 
@@ -34,7 +34,7 @@ contract SpenderPermit2ERC20Test is Test, PermitSignature {
 
         router = new Router();
         spender = new SpenderPermit2ERC20(address(router), permit2Addr);
-        mockERC20 = new MockERC20('Mock ERC20', 'mERC20');
+        mockERC20 = new ERC20('Mock ERC20', 'mERC20');
         DOMAIN_SEPARATOR = EIP712(permit2Addr).DOMAIN_SEPARATOR();
 
         // User approved spender and permit2
