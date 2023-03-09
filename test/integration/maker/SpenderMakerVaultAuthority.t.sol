7f42312db4be6674fdd68f500c40ac7cba652c7e
Mussina Lin <mussinalin@gmail.com>
2023-03-10 01:53:07 +0800

    feat: move maker tests to ./integration/maker. Add MakerCommonUtils.sol

--- test/integration/maker/SpenderMakerVaultAuthority.t.sol
+++ test/integration/maker/SpenderMakerVaultAuthority.t.sol
@@ -3,31 +3,16 @@ pragma solidity ^0.8.0;
 
 import {Test} from 'forge-std/Test.sol';
 import {IERC20} from 'openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol';
-import {SpenderMakerVaultAuthority, ISpenderMakerVaultAuthority} from '../../src/SpenderMakerVaultAuthority.sol';
-import {Router, IRouter} from '../../src/Router.sol';
-import {IAgent} from '../../src/interfaces/IAgent.sol';
-import {IParam} from '../../src/interfaces/IParam.sol';
-import {IDSProxy, IDSProxyRegistry} from '../../src/interfaces/maker/IDSProxy.sol';
-import {IMakerManager, IMakerVat} from '../../src/interfaces/maker/IMaker.sol';
+import {SpenderMakerVaultAuthority, ISpenderMakerVaultAuthority} from '../../../src/SpenderMakerVaultAuthority.sol';
+import {Router, IRouter} from '../../../src/Router.sol';
+import {IAgent} from '../../../src/interfaces/IAgent.sol';
+import {IParam} from '../../../src/interfaces/IParam.sol';
+import {IDSProxy, IDSProxyRegistry} from '../../../src/interfaces/maker/IDSProxy.sol';
+import {IMakerManager, IMakerVat} from '../../../src/interfaces/maker/IMaker.sol';
+import {MakerCommonUtils} from '../../utils/MakerCommonUtils.sol';
 
-contract SpenderMakerVaultAuthorityTest is Test {
+contract SpenderMakerVaultAuthorityTest is Test, MakerCommonUtils {
     address public constant NATIVE = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    address public constant LINK_TOKEN = 0x514910771AF9Ca656af840dff83E8264EcF986CA;
-
-    // MCD contract address
-    address public constant PROXY_REGISTRY = 0x4678f0a6958e4D2Bc4F1BAF7Bc52E8F3564f3fE4;
-    address public constant CDP_MANAGER = 0x5ef30b9986345249bc32d8928B7ee64DE9435E39;
-    address public constant VAT = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
-    address public constant PROXY_ACTIONS = 0x82ecD135Dce65Fbc6DbdD0e4237E0AF93FFD5038;
-    address public constant JUG = 0x19c0976f590D67707E62397C87829d896Dc0f1F1;
-    address public constant ETH_JOIN_A = 0x2F0b23f53734252Bda2277357e97e1517d6B042A;
-    address public constant DAI_JOIN = 0x9759A6Ac90977b93B58547b4A71c78317f391A28;
-    address public constant GEM_JOIN_LINK_A = 0xdFccAf8fDbD2F4805C174f856a317765B49E4a50;
-    address public constant DAI_TOKEN = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-
-    address public constant GEM = LINK_TOKEN;
-    string public constant ETH_JOIN_NAME = 'ETH-A';
-    string public constant TOKEN_JOIN_NAME = 'LINK-A';
     uint256 public constant DRAW_DAI_AMOUNT = 20000 ether;
 
     ISpenderMakerVaultAuthority public spenderMaker;
@@ -112,15 +97,8 @@ contract SpenderMakerVaultAuthorityTest is Test {
         vm.label(address(spenderMaker), 'SpenderMaker');
         vm.label(address(spenderMakerDSProxy), 'SpenderMakerDSProxy');
         vm.label(address(userDSProxy), 'UserDSProxy');
-        vm.label(PROXY_REGISTRY, 'PROXY_REGISTRY');
-        vm.label(CDP_MANAGER, 'CDP_MANAGER');
-        vm.label(VAT, 'VAT');
-        vm.label(PROXY_ACTIONS, 'PROXY_ACTIONS');
-        vm.label(DAI_TOKEN, 'DAI_TOKEN');
-        vm.label(JUG, 'JUG');
-        vm.label(ETH_JOIN_A, 'ETH_JOIN_A');
-        vm.label(DAI_JOIN, 'DAI_JOIN');
-        vm.label(GEM, 'GEM');
+
+        makerCommonSetUp();
     }
 
     function testFreeETH() external {
--- test/integration/maker/UtilityMaker.t.sol
+++ test/integration/maker/UtilityMaker.t.sol
@@ -10,33 +10,18 @@ import {IParam} from '../../../src/interfaces/IParam.sol';
 import {IDSProxy, IDSProxyRegistry} from '../../../src/interfaces/maker/IDSProxy.sol';
 import {IMakerManager} from '../../../src/interfaces/maker/IMaker.sol';
 import {SpenderPermitUtils} from '../../utils/SpenderPermitUtils.sol';
+import {MakerCommonUtils} from '../../utils/MakerCommonUtils.sol';
 import {SafeCast160} from 'permit2/libraries/SafeCast160.sol';
 
 interface IMakerVat {
     function ilks(bytes32) external view returns (uint256 art, uint256 rate, uint256 spot, uint256 line, uint256 dust);
 }
 
-contract UtilityMakerTest is Test, SpenderPermitUtils {
+contract UtilityMakerTest is Test, MakerCommonUtils, SpenderPermitUtils {
     using SafeCast160 for uint256;
 
     uint256 public constant SKIP = type(uint256).max;
     address public constant NATIVE = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    address public constant LINK_TOKEN = 0x514910771AF9Ca656af840dff83E8264EcF986CA;
-
-    // MCD contract address
-    address public constant PROXY_REGISTRY = 0x4678f0a6958e4D2Bc4F1BAF7Bc52E8F3564f3fE4;
-    address public constant CDP_MANAGER = 0x5ef30b9986345249bc32d8928B7ee64DE9435E39;
-    address public constant VAT = 0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B;
-    address public constant PROXY_ACTIONS = 0x82ecD135Dce65Fbc6DbdD0e4237E0AF93FFD5038;
-    address public constant JUG = 0x19c0976f590D67707E62397C87829d896Dc0f1F1;
-    address public constant ETH_JOIN_A = 0x2F0b23f53734252Bda2277357e97e1517d6B042A;
-    address public constant DAI_JOIN = 0x9759A6Ac90977b93B58547b4A71c78317f391A28;
-    address public constant GEM_JOIN_LINK_A = 0xdFccAf8fDbD2F4805C174f856a317765B49E4a50;
-    address public constant DAI_TOKEN = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-
-    address public constant GEM = LINK_TOKEN;
-    string public constant ETH_JOIN_NAME = 'ETH-A';
-    string public constant TOKEN_JOIN_NAME = 'LINK-A';
 
     address public user;
     uint256 public userPrivateKey;
@@ -73,15 +58,8 @@ contract UtilityMakerTest is Test, SpenderPermitUtils {
         vm.label(address(utilityMaker), 'UtilityMaker');
         vm.label(address(utilityMakerDSProxy), 'UtilityMakerDSProxy');
         vm.label(address(spender), 'SpenderPermit2ERC20');
-        vm.label(PROXY_REGISTRY, 'PROXY_REGISTRY');
-        vm.label(CDP_MANAGER, 'CDP_MANAGER');
-        vm.label(VAT, 'VAT');
-        vm.label(PROXY_ACTIONS, 'PROXY_ACTIONS');
-        vm.label(DAI_TOKEN, 'DAI_TOKEN');
-        vm.label(JUG, 'JUG');
-        vm.label(ETH_JOIN_A, 'ETH_JOIN_A');
-        vm.label(DAI_JOIN, 'DAI_JOIN');
-        vm.label(GEM, 'GEM');
+
+        makerCommonSetUp();
     }
 
     function testOpenLockETHAndDraw(uint256 ethLockAmount, uint256 daiDrawAmount) external {
