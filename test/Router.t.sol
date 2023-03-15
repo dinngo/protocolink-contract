9be4947d872a98ea6afcb9a26c9b7f265b206507
ZD Hu <zd@dinngo.co>
2023-03-15 12:04:36 +0800

    Add execute with signer's signature to Router

--- test/Router.t.sol
+++ test/Router.t.sol
@@ -6,16 +6,19 @@ import {ERC20} from 'openzeppelin-contracts/contracts/token/ERC20/ERC20.sol';
 import {SafeERC20, IERC20} from 'openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol';
 import {Router, IRouter} from '../src/Router.sol';
 import {IParam} from '../src/interfaces/IParam.sol';
-import {ICallback, MockCallback} from './mocks/MockCallback.sol';
 import {MockFallback} from './mocks/MockFallback.sol';
+import {LogicSignature} from './utils/LogicSignature.sol';
 
-contract RouterTest is Test {
+contract RouterTest is Test, LogicSignature {
     using SafeERC20 for IERC20;
 
-    uint256 public constant BPS_BASE = 10_000;
     uint256 public constant SKIP = type(uint256).max;
+    uint256 public constant INVALID_REFERRAL = 0;
+    uint256 public constant SIGNER_REFERRAL = 1;
 
     address public user;
+    address public signer;
+    uint256 public signerPrivateKey;
     IRouter public router;
     IERC20 public mockERC20;
     address public mockTo;
@@ -23,17 +26,21 @@ contract RouterTest is Test {
     // Empty arrays
     address[] tokensReturnEmpty;
     IParam.Input[] inputsEmpty;
+    IParam.Logic[] logicsEmpty;
 
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event SignerAdded(address indexed signer, uint256 referral);
+    event SignerRemoved(address indexed signer);
 
     function setUp() external {
         user = makeAddr('User');
+        (signer, signerPrivateKey) = makeAddrAndKey('Signer');
 
         router = new Router();
         mockERC20 = new ERC20('mockERC20', 'mock');
         mockTo = address(new MockFallback());
 
         vm.label(address(mockERC20), 'mERC20');
+        vm.label(address(mockTo), 'mTo');
     }
 
     function testNewAgent() external {
@@ -94,18 +101,10 @@ contract RouterTest is Test {
     }
 
     function testCannotExecuteReentrance() external {
-        IParam.Logic[] memory callback = new IParam.Logic[](1);
-        callback[0] = IParam.Logic(
-            address(mockTo), // to
-            '',
-            inputsEmpty,
-            address(0), // approveTo
-            address(0) // callback
-        );
         IParam.Logic[] memory logics = new IParam.Logic[](1);
         logics[0] = IParam.Logic(
             address(router), // to
-            abi.encodeCall(IRouter.execute, (callback, tokensReturnEmpty)),
+            abi.encodeCall(IRouter.execute, (logicsEmpty, tokensReturnEmpty)),
             inputsEmpty,
             address(0), // approveTo
             address(0) // callback
@@ -139,5 +138,108 @@ contract RouterTest is Test {
     function checkExecutingAgent(address agent) external view {
         address executingAgent = router.getAgent();
         if (agent != executingAgent) revert();
+    }
+
+    function testAddSigner(address signer_, uint256 referral) external {
+        vm.assume(referral != INVALID_REFERRAL);
+        vm.expectEmit(true, true, true, true, address(router));
+        emit SignerAdded(signer_, referral);
+        router.addSigner(signer_, referral);
+        assertEq(router.signerReferrals(signer_), referral);
+    }
+
+    function testCannotAddSignerByNonOwner() external {
+        vm.expectRevert('Ownable: caller is not the owner');
+        vm.prank(user);
+        router.addSigner(signer, SIGNER_REFERRAL);
+    }
+
+    function testCannotAddSignerInvalidReferral() external {
+        vm.expectRevert(abi.encodeWithSelector(IRouter.InvalidReferral.selector, INVALID_REFERRAL));
+        router.addSigner(signer, INVALID_REFERRAL);
+    }
+
+    function testRemoveSigner(address signer_, uint256 referral) external {
+        vm.assume(referral != INVALID_REFERRAL);
+        router.addSigner(signer_, referral);
+        vm.expectEmit(true, true, true, true, address(router));
+        emit SignerRemoved(signer_);
+        router.removeSigner(signer_);
+        assertEq(router.signerReferrals(signer_), INVALID_REFERRAL);
+    }
+
+    function testCannotRemoveSignerByNonOwner() external {
+        vm.expectRevert('Ownable: caller is not the owner');
+        vm.prank(user);
+        router.removeSigner(signer);
+    }
+
+    function testExecuteWithSignature() external {
+        router.addSigner(signer, SIGNER_REFERRAL);
+
+        // Ensure correct EIP-712 encodeData for non-empty Input and Logic
+        IParam.Input[] memory inputs = new IParam.Input[](1);
+        inputs[0] = IParam.Input(
+            address(mockERC20),
+            SKIP, // amountBps
+            0 // amountOrOffset
+        );
+        IParam.Logic[] memory logics = new IParam.Logic[](1);
+        logics[0] = IParam.Logic(
+            address(mockTo),
+            '',
+            inputs,
+            address(0), // approveTo
+            address(0) // callback
+        );
+        uint256 deadline = block.timestamp;
+        IParam.LogicBatch memory logicBatch = IParam.LogicBatch(logics, deadline);
+        bytes memory sigature = getLogicBatchSignature(logicBatch, signerPrivateKey, router.domainSeparator());
+
+        vm.prank(user);
+        router.executeWithSignature(logicBatch, signer, sigature, tokensReturnEmpty);
+    }
+
+    function testCannotExecuteSignatureExpired() external {
+        uint256 deadline = block.timestamp - 1; // Expired
+        IParam.LogicBatch memory logicBatch = IParam.LogicBatch(logicsEmpty, deadline);
+        bytes memory sigature = getLogicBatchSignature(logicBatch, signerPrivateKey, router.domainSeparator());
+
+        vm.expectRevert(abi.encodeWithSelector(IRouter.SignatureExpired.selector, deadline));
+        vm.prank(user);
+        router.executeWithSignature(logicBatch, signer, sigature, tokensReturnEmpty);
+    }
+
+    function testCannotExecuteInvalidSigner() external {
+        // Don't add signer
+        uint256 deadline = block.timestamp;
+        IParam.LogicBatch memory logicBatch = IParam.LogicBatch(logicsEmpty, deadline);
+        bytes memory sigature = getLogicBatchSignature(logicBatch, signerPrivateKey, router.domainSeparator());
+
+        vm.expectRevert(abi.encodeWithSelector(IRouter.InvalidSigner.selector, signer));
+        vm.prank(user);
+        router.executeWithSignature(logicBatch, signer, sigature, tokensReturnEmpty);
+    }
+
+    function testCannotExecuteInvalidSignature() external {
+        router.addSigner(signer, SIGNER_REFERRAL);
+
+        // Sign correct deadline and logicBatch
+        uint256 deadline = block.timestamp;
+        IParam.LogicBatch memory logicBatch = IParam.LogicBatch(logicsEmpty, deadline);
+        bytes memory sigature = getLogicBatchSignature(logicBatch, signerPrivateKey, router.domainSeparator());
+
+        // Tamper deadline
+        logicBatch = IParam.LogicBatch(logicsEmpty, deadline + 1);
+        vm.prank(user);
+        vm.expectRevert(IRouter.InvalidSignature.selector);
+        router.executeWithSignature(logicBatch, signer, sigature, tokensReturnEmpty);
+
+        // Tamper logics
+        IParam.Logic[] memory logics = new IParam.Logic[](1);
+        logicBatch = IParam.LogicBatch(logics, deadline);
+        vm.prank(user);
+        vm.expectRevert(IRouter.InvalidSignature.selector);
+        router.executeWithSignature(logicBatch, signer, sigature, tokensReturnEmpty);
     }
 }
