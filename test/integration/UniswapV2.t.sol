ed09235cf3c40d4b4df54abe3dac992fe3700c62
ZD Hu <zd@dinngo.co>
2023-03-24 16:39:57 +0800

    Restore assertApproxEqRel ratio

--- test/integration/UniswapV2.t.sol
+++ test/integration/UniswapV2.t.sol
@@ -212,7 +212,7 @@ contract UniswapV2Test is Test, SpenderPermitUtils {
         assertEq(tokenIn1.balanceOf(address(agent)), 0);
         assertEq(tokenOut.balanceOf(address(router)), 0);
         assertEq(tokenOut.balanceOf(address(agent)), 0);
-        assertApproxEqRel(tokenIn0.balanceOf(user), amountIn0, 0.03 * 1e18);
+        assertApproxEqRel(tokenIn0.balanceOf(user), amountIn0, 0.01 * 1e18);
     }
 
     function _logicUniswapV2SwapNativeToToken(
