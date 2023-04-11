b0ba0dec66b40bc4be1bf3db9d1218666e37b902
benhuang <ben@dinngo.co>
2023-04-11 14:46:51 +0800

    Fix SPDX

--- src/utility/UtilityMaker.sol
+++ src/utility/UtilityMaker.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
 import {SafeERC20, IERC20} from 'openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol';
