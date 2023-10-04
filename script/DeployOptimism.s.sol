88f062909d709f72dc8359d1a0a18cad34020c1c
ZD Hu <zd@dinngo.co>
2023-10-04 17:45:58 +0800

    Add balancer callback deployment on op

--- script/DeployOptimism.s.sol
+++ script/DeployOptimism.s.sol
@@ -4,8 +4,14 @@ pragma solidity ^0.8.0;
 import {DeployCREATE3Factory} from './DeployCREATE3Factory.s.sol';
 import {DeployRouter} from './DeployRouter.s.sol';
 import {DeployAaveV3FlashLoanCallback} from './callbacks/DeployAaveV3FlashLoanCallback.s.sol';
+import {DeployBalancerV2FlashLoanCallback} from './callbacks/DeployBalancerV2FlashLoanCallback.s.sol';
 
-contract DeployOptimism is DeployCREATE3Factory, DeployRouter, DeployAaveV3FlashLoanCallback {
+contract DeployOptimism is
+    DeployCREATE3Factory,
+    DeployRouter,
+    DeployAaveV3FlashLoanCallback,
+    DeployBalancerV2FlashLoanCallback
+{
     address public constant DEPLOYER = 0xDdbe07CB6D77e81802C55bB381546c0DA51163dd;
 
     /// @notice Set up deploy parameters and deploy contracts whose `deployedAddress` equals `UNDEPLOYED`.
@@ -32,6 +38,12 @@ contract DeployOptimism is DeployCREATE3Factory, DeployRouter, DeployAaveV3Flash
             aaveV3Provider: 0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb,
             feeRate: 5
         });
+
+        balancerV2FlashLoanCallbackConfig = BalancerV2FlashLoanCallbackConfig({
+            deployedAddress: 0x08b3d2c773C08CF21746Cf16268d2E092881c208,
+            balancerV2Vault: 0xBA12222222228d8Ba445958a75a0704d566BF2C8,
+            feeRate: 5
+        });
     }
 
     function _run() internal override {
@@ -43,5 +55,6 @@ contract DeployOptimism is DeployCREATE3Factory, DeployRouter, DeployAaveV3Flash
 
         // callback
         _deployAaveV3FlashLoanCallback(deployedCreate3FactoryAddress, deployedRouterAddress);
+        _deployBalancerV2FlashLoanCallback(deployedCreate3FactoryAddress, deployedRouterAddress);
     }
 }
